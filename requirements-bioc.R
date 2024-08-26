33cf98e9b9da5090e3e76600779c1d45525f3ffc
Jonathan Talbot-Martin <jtalbotmartin@outlook.com>
2024-08-26 18:42:19 +0100

    initial container build

---
+++ requirements-bioc.R
@@ -0,0 +1,7 @@
+bioc_pkgs<-c(
+"doMC", "doRNG","doSNOW", "mixtools", "SummarizedExperiment", "SingleCellExperiment", "DESeq2"
+)
+
+requireNamespace("BiocManager")
+BiocManager::install(bioc_pkgs,ask=FALSE)
+BiocManager::install("scater", ask=FALSE)
